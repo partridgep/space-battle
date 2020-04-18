bcfe6d25e2459f7f5321fb18763b746b0a289121
partridgep <paul.partridg@gmail.com>
2020-04-18 15:58:12 -0400

    cleans up code and adds comments

--- script.js
+++ script.js
@@ -35,7 +35,9 @@ const game = {
                 {console.log(`${attacked.name} has been DESTROYED!`);
                 //remove first alien ship out of array
                 this.alienShips.shift();
+                //check if there are any remaining enemy ships
                 if (this.alienShips.length > 0) {
+                    //prompt user if they want to attack next ship or retreat
                     let answer = prompt('Would you like to attack the next ship or retreat?');
                     //if answer is retreat, game is over
                     if (answer.toLowerCase() === 'retreat')
@@ -43,10 +45,12 @@ const game = {
                         //exit out of game
                         this.playing = false;}
                     else if (answer.toLowerCase() === 'attack') {
+                        //attack next alien ship
                         this.attack(attacker, this.alienShips[0]);
                     }
                     }
                     else {
+                        //exit out of game after hard earned victory
                         console.log('You saved Earth!');
                         this.playing = false;
                     }
@@ -71,51 +75,35 @@ const game = {
 
     //generate enemy ships function
     generateEnemies() {
+        //iterate over list of alien ship names
         for(ship of this.alienShipNames) {
+            //for each alien ship name:
+            //give it its name
             let name = ship;
+            //randomize its hull number between 3 and 6
             let hull = (Math.floor(Math.random() * (6-3+1) + 3));
+            //randomize its firepower between 2 and 4
             let firepower = (Math.floor(Math.random() * (4-2+1) + 2));
+            //randomize its accuracy between 0.6 and 0.8
             let accuracy = Math.random() * (0.8-0.6) + 0.6;
+            //create a new Ship object with those values
             let alienShip = new Ship(ship, hull, firepower, accuracy);
+            //add that object to the array of alien ships
             this.alienShips.push(alienShip);
         }
     },
     
     //play function
     play: function() {
+        //get our array of enemies
         this.generateEnemies();
+        //check the conditional loop
         while (this.playing === true) {
+                //start the game by attacking first alien ship
                 this.attack(this.ussAssembly, this.alienShips[0]);
         }
     }
 }
 
-//game.generateEnemies();
-//console.log(game.alienShips[0]);
+
 console.log(game.play());
-
-
-//console.log(game.attack(game.alienShip, game.ussAssembly));
-
-
-
-    /*
-    alienShips = ['Millenium Falcon'],
-
-    //generate our ship
-    generateUSS: function () {
-        let UssAssembly = new Ship('USS Assembly', 20, 5, 0.7);
-        return UssAssembly;
-    },
-
-    //generate alien ships
-    generateAlienShips: function() {
-        for(let ship of alienShips) {
-            let alienShip = new Ship(ship, 5, 4, 0.5);
-        }
-    return alienShips;
-    }
-    */
-
-
-//console.log(game.alienShip);
